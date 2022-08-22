4dd8bb3d76f7cb78e31558193e76ae36b3802ecc
Tajh Alexander <82831879+RMRKBL@users.noreply.github.com>
2022-08-22 00:01:14 -0400

    Add files via upload

---
+++ MovieFight/utils.js
@@ -0,0 +1,12 @@
+// Delay key press search results after final key press by 1s then return requested data
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
